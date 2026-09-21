d3f1e83c016bc620dab25e0424d4257bb0fc21b7
scottish1616 <kisakalevi15@gmail.com>
2026-09-21 11:28:50 +0300

    fixed session and access

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://richykicks.onrender.com/api/:path*",
-      },
-    ];
-  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
